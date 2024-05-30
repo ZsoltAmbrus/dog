b376dad15dbfa7a898d5e5aed21623c526ab0d0e
Ambrus <ambruszsolt@ktch.hu>
2024-05-30 08:42:58 +0200

    jo

--- app01/app/app.js
+++ app01/app/app.js
@@ -1,9 +1,9 @@
 function calculateEllipsoidVolume(a, b, c) {
     return (4 / 3) * Math.PI * a * b * c;
 }
-var a = parseFloat(prompt("Enter the length of the first axis (a):") || "0");
-var b = parseFloat(prompt("Enter the length of the second axis (b):") || "0");
-var c = parseFloat(prompt("Enter the length of the third axis (c):") || "0");
+var a = parseFloat(prompt("Írd be az első lehetőséget (a):") || "0");
+var b = parseFloat(prompt("Írd be a második lehetőséget (b):") || "0");
+var c = parseFloat(prompt("Írd be a harmadik lehetőséget (c):") || "0");
 var volume = calculateEllipsoidVolume(a, b, c);
-console.log("The volume of the ellipsoid is: ".concat(volume));
-alert("The volume of the ellipsoid is: ".concat(volume));
+console.log("A végeredmény: ".concat(volume));
+alert("A végeredmény: ".concat(volume));
